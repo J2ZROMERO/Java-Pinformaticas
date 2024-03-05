880cfd3a23b173ce9837d52139e0746c6cb5268e
J2ZROMERO <jose-zromero@hotmail.com>
2024-03-04 22:49:06 -0600

    Update passwrod generetor code

--- main/java/com/poo/classes_objects/generador_contrasenas/Password.java
+++ main/java/com/poo/classes_objects/generador_contrasenas/Password.java
@@ -5,28 +5,50 @@ import java.util.Random;
 public class Password {
     public static void main(String[] args) {
 
+        Password p = new Password();
+        p.setTamanoArray(5);
+        p.setTamanoPass(4);
 
-        String totalPass = "";
-        Random rnd = new Random();
-
-        for (int j = 0; j < 4; j++) {
-
-            totalPass +=  (""+(Math.abs(rnd.nextInt((122-97)+1)+97))).substring(0,2);
-
-            for (int i = 0; i < 2 ; i++) {
-
-                totalPass +=  (char)(Math.abs(rnd.nextInt((122-97)+1)+97));
-            }
-
-            totalPass +=  (char)(Math.abs(rnd.nextInt((90-65)+1)+65));
-            totalPass +=  "-";
+        for (int i = 0; i < p.tamanoArray(); i++) {
+            System.out.println(p.generarPassword()[i]);
         }
 
-
-
-
-
-
-        System.out.println(totalPass.substring(0,totalPass.length()-1));
     }
+
+
+    public String []generarPassword(){
+        String generatedPass = "";
+        Random rnd = new Random();
+
+        for (int j = 0; j < tamanoArray(); j++) {
+            generatedPass += (""+(Math.abs(rnd.nextInt()))).substring(0,1)
+            +""+
+                    (char)(Math.abs(rnd.nextInt((122-97)+1)+97))+
+                    (char)(Math.abs(rnd.nextInt((90-65)+1)+65));
+            arrayPasswords[j] = generatedPass.substring(0);
+            generatedPass = "";
+        }
+
+    return arrayPasswords;
+    }
+
+
+    public void setTamanoPass(int tamano){
+        this.longitudPass = tamano;
+    }
+
+    public void setTamanoArray(int tamano){
+
+        arrayPasswords = new String[tamano];
+    }
+
+public int tamanoArray(){
+        return arrayPasswords.length;
+}
+
+/*public int longitudPasswrord(){
+        return totalPass.length;
+    }*/
+    private String[] arrayPasswords;
+    private int longitudPass;
 }

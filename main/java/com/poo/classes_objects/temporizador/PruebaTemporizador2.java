3306cde815ba568c1cb36f2260810aec85fff990
jose.zromero <jose-zromero@hotmail.com>
2024-09-15 14:44:46 -0600

    Add inner classes

---
+++ main/java/com/poo/classes_objects/temporizador/PruebaTemporizador2.java
@@ -0,0 +1,39 @@
+package com.poo.classes_objects.temporizador;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Date;
+
+public class PruebaTemporizador2 {
+    public static void main(String[] args) {
+        Reloj rolex = new Reloj();
+        rolex.ejecutarIntervalo(5,true);
+        JOptionPane.showMessageDialog(null,"estamos impriendo");
+    }
+
+}
+
+class Reloj {
+
+
+    public void ejecutarIntervalo(int intervalo,boolean sonido){
+
+         class DameLaHora implements ActionListener{
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Date ahora  = new Date();
+                System.out.println("la hora es cada 23 segundos" + ahora);
+                if(sonido) Toolkit.getDefaultToolkit().beep();
+            }
+        }
+
+        ActionListener oyente = new DameLaHora();
+        Timer miTemporizador = new Timer(intervalo,oyente);
+        miTemporizador.start();
+    }
+
+
+}
